6424c3efa92a25e8f334db2235a3b1d50cb44066
thmkhtry <taha490mokh@gmail.com>
2025-07-31 13:52:37 +0330

    add str method in model

--- taskmgr/models.py
+++ taskmgr/models.py
@@ -3,4 +3,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=800)
     text = models.TextField(blank=True, null=True,max_length=1800)
-    create_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
