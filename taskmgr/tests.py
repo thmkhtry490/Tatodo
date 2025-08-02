2994eb2d4f660ef8e0547a558509afbb77ff8f9e
thmkhtry <taha490mokh@gmail.com>
2025-08-02 22:12:27 +0330

    add test for models

--- taskmgr/tests.py
+++ taskmgr/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
-# Create your tests here.
+from .models import Task
+class TaskTest(TestCase):
+    def setUp(self):
+        Task.objects.create(title="testing",text="the test part",done=False)
+    def test_first(self):
+       self.task = Task.objects.get(title="testing")
+       self.assertEqual(str(self.task),"testing")
