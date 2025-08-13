d6a93ee58775dad0d7de1f66299e67d1afd764bf
thmkhtry <taha490mokh@gmail.com>
2025-08-13 10:44:02 +0330

    install apis

--- mytodolist/settings.py
+++ mytodolist/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taskmgr'
+    'taskmgr',
+    'apis',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
