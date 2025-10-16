d2d637e95c252f78c4dbacb7f62b507d897ee0e5
thmkhtry <taha490mokh@gmail.com>
2025-10-16 17:19:19 +0330

    Add AdminLTE template and remove task model from Admin

--- mytodolist/settings.py
+++ mytodolist/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
